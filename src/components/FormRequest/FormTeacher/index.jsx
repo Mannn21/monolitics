7cdc82efdc1575fe154c1855fc38b5a656f1c4ef
Mannn21 <aimannurrofi21@gmail.com>
2023-09-01 07:50:37 +0700

    update form request

--- src/components/FormRequest/FormTeacher/index.jsx
+++ src/components/FormRequest/FormTeacher/index.jsx
@@ -1,30 +1,43 @@
 "use client";
 
+import { useState, useRef } from "react";
 import { Button, Label, TextInput, Select, Textarea } from "flowbite-react";
-import { BiSolidUser } from "react-icons/bi";
 import styled from "./index.module.css";
 
 export default function FormTeacher() {
+	const [request, setRequest] = useState({
+		name: "Aimanurrofi",
+		idUser: "9247-4786-12487-212-42",
+		type: "",
+		description: "",
+	});
+
+	const nameRef = useRef(request.name);
+	const idRef = useRef(request.idUser);
+	const typeRef = useRef(request.type);
+	const descriptionRef = useRef(request.description);
 
 	return (
-		<form className="flex max-w-md flex-col gap-4">
-			<div className="max-w-md">
+		<form className="flex max-w flex-col gap-4">
+			<div className="max-w">
 				<div className="mb-2 block">
 					<Label htmlFor="username" value="Masukkan username" />
 				</div>
 				<TextInput
 					disabled
+					ref={nameRef}
 					id="username"
 					placeholder="Aimanurrofi"
 					required
 					type="text"
 				/>
 			</div>
-			<div className="max-w-md">
+			<div className="max-w">
 				<div className="mb-2 block">
 					<Label htmlFor="id-user" value="Masukkan Id User" />
 				</div>
 				<TextInput
+					ref={idRef}
 					disabled
 					id="is-user"
 					placeholder="9247-4786-12487-212-42"
@@ -32,11 +45,18 @@ export default function FormTeacher() {
 					type="number"
 				/>
 			</div>
-			<div className="max-w-md" id="select">
+			<div className="max-w" id="select">
 				<div className="mb-2 block">
 					<Label htmlFor="request" value="Masukkan jenis permohonan" />
 				</div>
-				<Select id="request" required defaultValue="none">
+				<Select
+					id="request"
+					required
+					defaultValue="none"
+					ref={typeRef}
+					onChange={e =>
+						setRequest(prev => ({ ...prev, type: e.target.value }))
+					}>
 					<option value="none" disabled selected>
 						Pilih jenis permohonan
 					</option>
@@ -47,13 +67,15 @@ export default function FormTeacher() {
 				</Select>
 			</div>
 
-			<div className="max-w-md" id="textarea">
+			<div className="max-w" id="textarea">
 				<div className="mb-2 block">
-					<Label htmlFor="comment" value="Alasan Permohonan" />
+					<Label htmlFor="comment" value="Deskripsi Permohonan" />
 				</div>
 				<Textarea
+					ref={descriptionRef}
+					onChange={e => setRequest(prev => ({...prev, description: e.target.value}))}
 					id="comment"
-					placeholder="Masukkan alasan permohonan..."
+					placeholder="Masukkan deskripsi permohonan..."
 					required
 					rows={4}
 				/>
